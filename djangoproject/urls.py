2b464aa34a859b745d3a2d8f61bb4c6bba157d68
Anshuman Kumar <anshinanu@yahoo.com>
2021-01-12 21:26:26 +0530

    added some static files

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +24,5 @@ urlpatterns = [
     path('articles/', include('articles.urls')),
     path('', views.home),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
