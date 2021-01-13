220e430972941c3ffed95b1b8e4e382333f1a21a
Anshuman Kumar <anshinanu@yahoo.com>
2021-01-13 21:14:25 +0530

    added thumbnail feature

--- articles/models.py
+++ articles/models.py
@@ -6,9 +6,11 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    thumb = models.ImageField(default = 'defualt.png', blank=True)
 
     def __str__(self):
         return self.title
     
     def snippet(self):
         return (self.body[:50]+'...')
+        
