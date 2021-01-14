59c2bea11459251915f7bcde495aa177cc18cc81
Anshuman Kumar <anshinanu@yahoo.com>
2021-01-14 23:54:12 +0530

    feature: author mapped to articles

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=100)
@@ -7,6 +7,7 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default = 'defualt.png', blank=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.title
