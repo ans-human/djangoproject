89f54ef5f4159bb05167841d3ece1fc1a21422e9
Anshuman Kumar <anshinanu@yahoo.com>
2021-01-17 17:50:48 +0530

    d fix

--- articles/views.py
+++ articles/views.py
@@ -7,7 +7,7 @@ from . import forms
 # Create your views here.
 def articles_list(request):
     article = Article.objects.all().order_by('date')
-    return render(request, 'articles/articles_list.html', {'articles':article})
+    return render(request, 'articles/articles_list.html')
 
 def article_detail(request, slug):
     article = Article.objects.get(slug=slug)
