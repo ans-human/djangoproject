4d2aaa54cdcd072b6982e9f17e1edd02f661946f
Anshuman Kumar <anshinanu@yahoo.com>
2021-01-18 09:54:07 +0530

    dpmnt fix

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -87,6 +87,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'NAME_OF_DB',
+#         'USER': 'DB_USER_NAME',
+#         'PASSWORD': 'DB_PASSWORD',
+#         'HOST': 'localhost',
+#         'PORT': '5433',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
